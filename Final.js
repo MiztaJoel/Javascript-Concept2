4b1ffa7a9052e7c27e246fe6f7643373406b004c
joel <olugaabiola18@gmail.com>
2024-01-12 03:27:39 +0100

    Javascript Basic
    Learning the basic javascript concept for react language

---
+++ Final.js
@@ -0,0 +1,18 @@
+// console.log(fetch('https://jsonplaceholder.typicode.com/todos'))
+// fetch('https://jsonplaceholder.typicode.com/todos')
+// .then((res)=>res.json())
+// .then((data)=>console.log(data));
+
+console.log("jonas");
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+const todo = getTodos();
+console.log(todo);
+
+console.log("Jonas");
